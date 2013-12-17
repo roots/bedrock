e20d3a884c4f428b6e7952212faebb1bab62153a
Scott Walkinshaw <scott.walkinshaw@gmail.com>
2013-12-16 23:08:34 -0500

    Set DB_HOST default

--- config/environments/production.php
+++ config/environments/production.php
@@ -3,7 +3,7 @@
 define('DB_NAME', $_SERVER['DB_NAME']);
 define('DB_USER', $_SERVER['DB_USER']);
 define('DB_PASSWORD', $_SERVER['DB_PASSWORD']);
-define('DB_HOST', $_SERVER['DB_HOST']);
+define('DB_HOST', isset($_SERVER['DB_HOST']) ? $_SERVER['WPDB_HOST_ENV'] : 'localhost');
 
 define('WP_HOME', $_SERVER['WP_HOME']);
 define('WP_SITEURL', $_SERVER['WP_SITEURL']);
