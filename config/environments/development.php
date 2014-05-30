b13438c4cec38d5893a93ab54277307e4ca0dec3
Christopher Lamm <chris@theantichris.com>
2014-05-30 16:02:04 -0400

    Added error display to dev config.

--- config/environments/development.php
+++ config/environments/development.php
@@ -8,6 +8,9 @@ define('DB_HOST', getenv('DB_HOST') ? getenv('DB_HOST') : 'localhost');
 define('WP_HOME', getenv('WP_HOME'));
 define('WP_SITEURL', getenv('WP_SITEURL'));
 
+ini_set('display_startup_errors',1);
+ini_set('display_errors',1);
+error_reporting(-1);
 define('SAVEQUERIES', true);
 define('WP_DEBUG', true);
 define('SCRIPT_DEBUG', true);
