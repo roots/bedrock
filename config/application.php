124d9708932b85a2570800df99dd8a8cffc66668
Michael Bianco <iloveitaly@gmail.com>
2014-06-01 13:48:20 -0400

    Add optional DB_PREFIX configuration option

--- config/application.php
+++ config/application.php
@@ -35,7 +35,7 @@ define('WP_CONTENT_URL', WP_HOME . CONTENT_DIR);
  */
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
-$table_prefix = 'wp_';
+$table_prefix = getenv('DB_PREFIX') ? getenv('DB_PREFIX') : 'wp_';
 
 /**
  * WordPress Localized Language
