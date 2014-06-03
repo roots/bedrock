d0c6538676e0ef56172eb5116f1d7003a0f61054
Christopher Lamm <chris@theantichris.com>
2014-06-03 14:07:25 -0400

    Updated Capistrano files.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,18 +1,9 @@
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'theantichris.com'
+set :repo_url, 'git@github.com:theantichris/theantichris.com.git'
 
-# Branch options
-# Prompts for the branch name (defaults to current branch)
-#ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
-
-# Sets branch to current one
-#set :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
-
-# Hardcodes branch to always be master
-# This could be overridden in a stage config file
 set :branch, :master
 
-set :deploy_to, "/srv/www/#{fetch(:application)}"
+set :deploy_to, "/var/www/#{fetch(:application)}"
 
 set :log_level, :info
 
@@ -23,12 +14,9 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :service, :nginx, :reload
+      execute :service, :nginx, :reload
     end
   end
 end
 
-# The above restart task is not run by default
-# Uncomment the following line to run it on deploys if needed
-# after 'deploy:publishing', 'deploy:restart'
+after 'deploy:publishing', 'deploy:restart'
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -8,17 +8,16 @@ set :stage, :staging
 
 # Extended Server Syntax
 # ======================
-server 'example.com', user: 'deploy', roles: %w{web app db}
+server 'staging.theantichris.dev', user: 'vagrant', roles: %w{web app db}
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
 # you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
 # set it globally
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
+set :ssh_options, {
+  forward_agent: false,
+  auth_methods: %w(vagrant)
+}
 
 fetch(:default_env).merge!(wp_env: :staging)
 
