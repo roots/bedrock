f1ea7b4aa3ea248c8e9aa812536d9dd245584ba7
Christopher Lamm <chris@theantichris.com>
2014-06-22 09:49:55 -0400

    Fixed block name.

--- puppet/modules/php/manifests/init.pp
+++ puppet/modules/php/manifests/init.pp
@@ -3,27 +3,19 @@ class php($webRoot) {
    'add-php5-repo':
    command => 'sudo add-apt-repository ppa:ondrej/php5',
    unless => 'ls /etc/apt/sources.list.d/ondrej-php5-precise.list',
-   notify => Exec['sudo apt-get update'],
+   notify => Exec['update-for-repo'],
    require => Package['python-software-properties'],
   }
 
   package {
     ['php5-fpm', 'php5-cli', 'php5-mysql']:
     ensure => present,
-    require => Exec['add-php5-repo', 'update-package-list'],
+    require => Exec['add-php5-repo', 'update-for-repo'],
   }
 
   service {
     'php5-fpm':
     ensure => running,
-    require => Package['php5-fpm'],
-  }
-
-  file {
-    '/etc/php5/fpm/pool.d/www.conf':
-    ensure => present,
-    source => 'puppet:///modules/php/www.conf',
-    notify => Service['php5-fpm'],
     require => Package['php5-fpm'],
   }
 }
