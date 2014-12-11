44da6711c08070c06f29f3f0174947f20abba967
Fabien Schurter <fabien.schurter@gmail.com>
2014-12-11 17:26:00 +0100

    Disable UI-driven updates in staging and production
    DISALLOW_FILE_MODS implicitly includes AUTOMATIC_UPDATER_DISABLED and
    DISALLOW_FILE_EDIT, plus it hides the update notifications in the admin
    bar and the "Updates" sub-menu in the "Dashboard" admin menu, and it
    also hides "Update Available" messages in "Appearance -> Themes" and
    "Plugins -> Installed Plugins".

--- config/environments/production.php
+++ config/environments/production.php
@@ -11,3 +11,4 @@ define('WP_SITEURL', getenv('WP_SITEURL'));
 ini_set('display_errors', 0);
 define('WP_DEBUG_DISPLAY', false);
 define('SCRIPT_DEBUG', false);
+define('DISALLOW_FILE_MODS', true); // this disables all file modifications including updates and update notifications
