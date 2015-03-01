af45de36e61b406820a164e73e2465c7898a6cb6
Scott Walkinshaw <scott.walkinshaw@gmail.com>
2015-02-28 22:25:35 -0600

    yee

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,7 @@
 $tests_dir = getenv('WP_TESTS_DIR');
 
 if (!$tests_dir) {
-  $tests_dir = '../tests-includes';
+  $tests_dir = '../../tests-includes';
 }
 
 require_once $tests_dir . '/includes/functions.php';
