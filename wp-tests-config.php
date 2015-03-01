ea7b274d8c6c92dbd0851228362885d8025b75fc
Scott Walkinshaw <scott.walkinshaw@gmail.com>
2015-03-01 00:05:14 -0600

    include test config

---
+++ wp-tests-config.php
@@ -0,0 +1,33 @@
+<?php
+
+if (file_exists('.env')) {
+  Dotenv::load();
+}
+
+/* Path to the WordPress codebase you'd like to test. Add a backslash in the end. */
+define('ABSPATH', dirname( __FILE__ ) . '/web/wp/');
+
+// Test with multisite enabled.
+// Alternatively, use the tests/phpunit/multisite.xml configuration file.
+// define( 'WP_TESTS_MULTISITE', true );
+
+// Force known bugs to be run.
+// Tests with an associated Trac ticket that is still open are normally skipped.
+// define( 'WP_TESTS_FORCE_KNOWN_BUGS', true );
+
+define('WP_DEBUG', true);
+
+define('DB_NAME', getenv('DB_NAME'));
+define('DB_USER', getenv('DB_USER'));
+define('DB_PASSWORD', getenv('DB_PASSWORD'));
+define('DB_HOST', getenv('DB_HOST') ? getenv('DB_HOST') : 'localhost');
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
+
+$table_prefix  = 'wptests_';
+
+define('WP_TESTS_DOMAIN', 'example.org');
+define('WP_TESTS_EMAIL', 'admin@example.org');
+define('WP_TESTS_TITLE', 'Test Blog');
+define('WP_PHP_BINARY', 'php');
+define('WPLANG', '');
