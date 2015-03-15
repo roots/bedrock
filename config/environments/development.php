5213172432d745814dad0baa94c76769fd689379
QWp6t <hi@qwp6t.me>
2015-03-15 00:51:10 -0700

    Add dd() function to dev environment

--- config/environments/development.php
+++ config/environments/development.php
@@ -11,3 +11,13 @@ define('WP_SITEURL', getenv('WP_SITEURL'));
 define('SAVEQUERIES', true);
 define('WP_DEBUG', true);
 define('SCRIPT_DEBUG', true);
+
+if (!function_exists('dd')) {
+  function dd() {
+    $xdebug = ini_get('xdebug.overload_var_dump');
+    $args = (array) func_get_args();
+    echo $xdebug ? '' : '<pre>';
+    echo call_user_func_array('_log', $args);
+    die($xdebug ? '' : '</pre>');
+  }
+}
