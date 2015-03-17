7ad6d23d211d5c1865701a4f8585a53f3f22a8b6
Rasmus <rasmusbe@users.noreply.github.com>
2015-03-17 11:29:44 +0100

    Added plugin meta for nicer output
    Added plugin meta for nicer output in Must use plugin listing.

--- web/app/mu-plugins/register-theme-directory.php
+++ web/app/mu-plugins/register-theme-directory.php
@@ -1,3 +1,12 @@
 <?php
+/*
+Plugin Name:  Register Theme Directory
+Plugin URI:   http://roots.io/wordpress-stack/
+Description:  Register default theme directory
+Version:      1.0.0
+Author:       Roots
+Author URI:   http://roots.io/
+License:      MIT License
+*/
 
 register_theme_directory(ABSPATH . 'wp-content/themes');
