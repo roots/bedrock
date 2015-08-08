58c074b4fa5803a1fdfcd65ba5b7a2750919a297
reidelliott <reid@efeqdev.com>
2015-08-07 22:20:16 -0500

    updates grid sample

--- web/app/themes/bonestrap/page-grid-sample.php
+++ web/app/themes/bonestrap/page-grid-sample.php
@@ -36,7 +36,7 @@ Template Name: Grid Sample
             <div class="col-md-4">.col-md-4</div>
           </div>
           <div class="row">
-            <div class="col-md-6">.col-md-6</div>
+            <div class="col-md-4 col-md-offset-1">.col-md-4</div>
             <div class="col-md-6">.col-md-6</div>
           </div>
         </div> <!-- /#grid-no-gutters -->
@@ -88,8 +88,8 @@ Template Name: Grid Sample
             <div class="col-md-1"><img src="http://placehold.it/400x400"></div>
           </div>
           <div class="row">
-            <div class="col-sm-8"><span>.col-md-8<br/><p>Preserve and cherish that pale blue dot permanence of the stars white dwarf, the only home we've ever known astonishment, concept of the number one stirred by starlight, bits of moving fluff tesseract inconspicuous motes of rock and gas, shores of the cosmic ocean cosmic ocean culture Vangelis something incredible is waiting to be known a billion trillion? Cambrian explosion. White dwarf across the centuries, cosmic fugue as a patch of light across the centuries great turbulent clouds a mote of dust suspended in a sunbeam. Muse about explorations.
-</p></span></div>
+            <div class="col-sm-8"><div class="div-padding"><span>.col-md-8<br/><p>Preserve and cherish that pale blue dot permanence of the stars white dwarf, the only home we've ever known astonishment, concept of the number one stirred by starlight, bits of moving fluff tesseract inconspicuous motes of rock and gas, shores of the cosmic ocean cosmic ocean culture Vangelis something incredible is waiting to be known a billion trillion? Cambrian explosion. White dwarf across the centuries, cosmic fugue as a patch of light across the centuries great turbulent clouds a mote of dust suspended in a sunbeam. Muse about explorations.
+</p></span></div></div>
             <div class="col-sm-4"><span>.col-md-4</span><h4>Sidebar</h4><img src="http://placehold.it/400x400"></div>
           </div>
           <div class="row">
