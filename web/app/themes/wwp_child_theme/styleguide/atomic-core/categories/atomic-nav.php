507d070edf6f8102bd3bc1767628595917e4746e
Lafay Marc <marc.lafay@wonderful.fr>
2016-08-29 10:24:13 +0200

    Màj style guide

---
+++ web/app/themes/wwp_child_theme/styleguide/atomic-core/categories/atomic-nav.php
@@ -0,0 +1,4 @@
+<?php include ('base/navItem-base.php');?>
+<?php include ('molecules/navItem-molecules.php');?>
+<?php include ('atoms/navItem-atoms.php');?>
+<?php include ('components/navItem-components.php');?>
