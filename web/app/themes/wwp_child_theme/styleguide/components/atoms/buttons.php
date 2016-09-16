a0d00c09037dd3b4636999ab89e8723e238099e5
Lafay Marc <marc.lafay@wonderful.fr>
2016-09-16 10:16:23 +0200

    Update style guide for buttons

--- web/app/themes/wwp_child_theme/styleguide/components/atoms/buttons.php
+++ web/app/themes/wwp_child_theme/styleguide/components/atoms/buttons.php
@@ -6,12 +6,7 @@
 <input class="btn btn-default" type="button" value="Input">
 <input class="btn btn-default" type="submit" value="Submit">
 <div class="btn-wrapper-inline">
-	<a class="btn btn-icon" href="#">
-	Button with icon
-	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 70.3 57.8">
-		<path d="M45.1 7.3H16.2c-1.6 0-2.8-1.3-2.8-2.8s1.3-2.8 2.8-2.8h28.9c1.6 0 2.8 1.3 2.8 2.8s-1.3 2.8-2.8 2.8zM53.4 55.9H3.8c-1.6 0-2.8-1.3-2.8-2.8v-36c0-1.6 1.3-2.8 2.8-2.8h49.6c1.6 0 2.8 1.3 2.8 2.8v8.3l8.2-5.7c.9-.6 2-.7 2.9-.2.9.5 1.5 1.5 1.5 2.5v24.5c0 1.1-.6 2-1.5 2.5s-2.1.4-2.9-.2l-8.2-5.7V53c0 1.6-1.2 2.9-2.8 2.9zM6.7 50.2h43.9V37.8c0-1.1.6-2 1.5-2.5s2.1-.4 2.9.2l8.2 5.7V27.5L55 33.2c-.9.6-2 .7-2.9.2-.9-.5-1.5-1.5-1.5-2.5V20H6.7v30.2z"/>
-	</svg>
-	</a>
+	<a class="btn btn-icon" href="#">Button with icon</a>
 </div>
 
 <hr>
