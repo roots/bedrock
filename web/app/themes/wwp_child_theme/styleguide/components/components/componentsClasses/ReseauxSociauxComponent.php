d22ce9f5d0822b398985161374598117c37ff4af
Jeremy Desvaux <jeremy.desvaux@wonderful.fr>
2016-09-20 10:05:06 +0200

    Réseaux sociaux dans le styleguide.

---
+++ web/app/themes/wwp_child_theme/styleguide/components/components/componentsClasses/ReseauxSociauxComponent.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jeremydesvaux
+ * Date: 20/09/2016
+ * Time: 09:29
+ */
+
+namespace WonderWp\Theme\Components;
+
+
+class ReseauxSociauxComponent extends AbstractComponent
+{
+    public function getMarkup()
+    {
+
+        $markup = '';
+
+        $reseauxOpts = array(
+            'facebook',
+            'twitter',
+            'youtube',
+            'instagram',
+            'pinterest',
+            'flickr'
+        );
+
+        $reseauxActifs = array();
+
+        foreach($reseauxOpts as $reseau){
+            $link = get_option('wonderwp_rs_'.$reseau);
+            if(!empty($link)){
+                $reseauxActifs[] = '<li class="'.$reseau.'"><a href="'.$link.'" target="_blank">'.__('rs.follow.'.$reseau,WWP_THEME_TEXTDOMAIN).'</a></li>';
+            }
+        }
+
+        if(!empty($reseauxActifs)){
+            $markup = '<ul class="reseaux-sociaux">
+                '.implode("\n",$reseauxActifs).'
+            </ul>';
+        }
+
+        return $markup;
+    }
+}
