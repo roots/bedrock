9c1b1c2bc880b955848696ad303aba0f069cfe14
Lafay Marc <marc.lafay@wonderful.fr>
2016-09-20 17:15:40 +0200

    Add spinner in style guide

---
+++ web/app/themes/wwp_child_theme/styleguide/components/atoms/loader.php
@@ -0,0 +1,20 @@
+<!--components/atoms/loader.php-->
+
+<span class="subTitle">Simple spinner</span>
+<div class="spinner">
+    <div class="bounce1"></div>
+    <div class="bounce2"></div>
+    <div class="bounce3"></div>
+</div>
+
+<hr>
+<span class="subTitle">Button + spinner</span>
+
+<button class="btn btn-default" type="button">
+    Bouton
+    <div class="spinner spinner-white">
+        <div class="bounce1"></div>
+        <div class="bounce2"></div>
+        <div class="bounce3"></div>
+    </div>
+</button>
