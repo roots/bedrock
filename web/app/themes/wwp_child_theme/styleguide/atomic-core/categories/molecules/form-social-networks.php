d4f921a0bb2d7789e7f34cf5df7128078eb8aa68
Lafay Marc <marc.lafay@wonderful.fr>
2016-09-20 16:29:19 +0200

    Add social networks molecule in style guide

--- web/app/themes/wwp_child_theme/styleguide/atomic-core/categories/molecules/form-social-networks.php
+++ web/app/themes/wwp_child_theme/styleguide/atomic-core/categories/molecules/form-social-networks.php
@@ -1,31 +1,31 @@
 <div class="aa_fileFormGroup">
-<label class="aa_label js-showHide-trigger"><span class="fa fa-plus"></span> <span class="aa_label__file">Rename</span> reseaux-sociaux</label>
+<label class="aa_label js-showHide-trigger"><span class="fa fa-plus"></span> <span class="aa_label__file">Rename</span> social-networks</label>
 <div class="showHide">
   <form id="form-rename-file"  class="aa_fileForm " action="/atomic-core/partial-mngr/file-rename.php" method="post">
       <div class="formInputGroup">
         <div class="inputBtnGroup">
           
           <button class="aa_btn aa_btn-pos" type="submit" >Rename</button>
-          <div class="inputBtnGroup__inputWrap"><input type="text" class="form-control" name="renameFileName" value="reseaux-sociaux" required></div>
+          <div class="inputBtnGroup__inputWrap"><input type="text" class="form-control" name="renameFileName" value="social-networks" required></div>
         </div>     
       </div>
       <input type="hidden" name="compDir" value="molecules"/>
       <input type="hidden" name="rename" value="rename"/>
-      <input type="hidden" name="oldName" value="reseaux-sociaux"/>
+      <input type="hidden" name="oldName" value="social-networks"/>
     </form>
 </div>
-<label class="aa_label js-showHide-trigger"><span class="fa fa-plus"></span> Change the <span class="aa_label__file">description</span> for reseaux-sociaux</label>
+<label class="aa_label js-showHide-trigger"><span class="fa fa-plus"></span> Change the <span class="aa_label__file">description</span> for social-networks</label>
 <div class="showHide">
    <form id="form-rename-notes"  class="aa_fileForm " action="/atomic-core/partial-mngr/notes-rename.php" method="post">
         <textarea class="form-control" name="compNotesNew"></textarea>        
         <button class="aa_btn aa_btn-pos" type="submit" >Update</button>
       <input type="hidden" name="compDir" value="molecules"/>
-      <input type="hidden" name="fileName" value="reseaux-sociaux"/>
+      <input type="hidden" name="fileName" value="social-networks"/>
       <input type="hidden" name="compNotes" value=""/>
       <input type="hidden" name="bgColor" value=""/>
     </form>
 </div>
-<label class="aa_label js-showHide-trigger"><span class="fa fa-plus"></span> Change the <span class="aa_label__file">contextual background color</span> for reseaux-sociaux</label>
+<label class="aa_label js-showHide-trigger"><span class="fa fa-plus"></span> Change the <span class="aa_label__file">contextual background color</span> for social-networks</label>
 <div class="showHide">
    <form id="form-change-bgColor"  class="aa_fileForm " action="/atomic-core/partial-mngr/bgcolor-rename.php" method="post">
         
@@ -34,12 +34,12 @@
         </div>      
         <button class="aa_btn aa_btn-pos" type="submit" >Update</button>
       <input type="hidden" name="compDir" value="molecules"/>
-      <input type="hidden" name="fileName" value="reseaux-sociaux"/>
+      <input type="hidden" name="fileName" value="social-networks"/>
       <input type="hidden" name="compNotes" value=""/>
       <input type="hidden" name="bgColor" value=""/>
     </form>
 </div>
-<label class="aa_label js-showHide-trigger"><span class="fa fa-plus"></span> <span class="aa_label__file">Move</span>  reseaux-sociaux</label>
+<label class="aa_label js-showHide-trigger"><span class="fa fa-plus"></span> <span class="aa_label__file">Move</span>  social-networks</label>
 <div class="showHide">
   <form id="form-file-move" class="aa_fileForm " action="/atomic-core/partial-mngr/file-move.php" method="post">
       <div class="formGroup">
@@ -69,19 +69,19 @@
         </div>
       </div>
       <input type="hidden" name="compDir" value="molecules"/>
-      <input type="hidden" name="fileMoveName" value="reseaux-sociaux"/>
+      <input type="hidden" name="fileMoveName" value="social-networks"/>
       <input type="hidden" name="moveFile" value="moveFile"/>
       <input type="hidden" name="compNotes" value=""/>
       <input type="hidden" name="bgColor" value=""/>
     </form>
 </div>
-<label class="aa_label js-showHide-trigger"><span class="fa fa-plus"></span> <span class="aa_label__file">Delete</span> reseaux-sociaux</label>
+<label class="aa_label js-showHide-trigger"><span class="fa fa-plus"></span> <span class="aa_label__file">Delete</span> social-networks</label>
 <div class="showHide">
     <form id="form-delete-file" class="aa_fileForm " action="/atomic-core/partial-mngr/delete.php" method="post">
       <div class="formInputGroup">
         <div class="inputBtnGroup">
           <button class="aa_btn aa_btn-neg" type="submit" >Delete</button>
-          <div class="inputBtnGroup__inputWrap"><input type="text" class="form-control" name="deleteFileName" placeholder="Must type reseaux-sociaux to delete"></div>
+          <div class="inputBtnGroup__inputWrap"><input type="text" class="form-control" name="deleteFileName" placeholder="Must type social-networks to delete"></div>
         </div>  
       </div>
       <input type="hidden" name="compDir" value="molecules"/>
