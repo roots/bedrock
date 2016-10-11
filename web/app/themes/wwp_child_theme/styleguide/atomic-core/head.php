72d321d445e4bb7302b4175892263ba1a155ebad
Lafay Marc <marc.lafay@wonderful.fr>
2016-10-11 12:27:59 +0200

    Suppression des imports de police non utilisés d'Atomic Docs Core

--- web/app/themes/wwp_child_theme/styleguide/atomic-core/head.php
+++ web/app/themes/wwp_child_theme/styleguide/atomic-core/head.php
@@ -17,9 +17,6 @@ require_once( get_stylesheet_directory().'/functions.php' );
 		<title></title>
 		<meta name="description" content="">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-
-       	<link href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,700' rel='stylesheet'>
-		<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
 		
 		<link rel="stylesheet" href="atomic-core/css/main.css">
 
