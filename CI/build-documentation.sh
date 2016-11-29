91aeddc439a15908d6b6a6f256a6c8906fe047ab
Jeremy Desvaux <jeremy.desvaux@wonderful.fr>
2016-11-29 18:25:17 +0100

    phpdoc / devdoc setup

--- CI/build-documentation.sh
+++ CI/build-documentation.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
 
+#ln -s /Users/jeremydesvaux/Sites/wonderwp/framework/ /Users/jeremydesvaux/Sites/wonderwp/site/vendor/wonderwp/framework
+
 echo "\n\n//==[ Build Step 2 : Build Documentation ]==========================\n";
-vendor/apigen/apigen/bin/apigen generate
+
+cd ../;
+
+echo "\n=> Generating PHPDOC";
+#vendor/apigen/apigen/bin/apigen generate
+
+echo "\n\n=> Generating Developer Documentation";
+echo "\n- Gathering Documentation Parts";
+rm -Rf web/documentation/dev/src/wonderwp_framework
+cp -R vendor/wonderwp/framework/documentation/ web/documentation/dev/src/wonderwp_framework
+cp -R web/app/themes/wonderwp_theme/documentation web/documentation/dev/src/wonderwp_theme
+echo "\n- Serving doc";
+vendor/jeremy-wdf/daux.io/bin/daux generate -s web/documentation/dev/src/ -d  web/documentation/dev/dest/ --delete
