5ff2d534d8d6d50c9a68c2e9f3052c3f4405890b
Thomas Hurd <thomas@pixels.fi>
2017-09-14 12:27:22 +0300

    update staging vars

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,9 +2,9 @@
 ###### Lukas Jakob Hafner - @saftsaak  ######
 ###### 2016.12.01 – v1.3               ######
 
-set :stage, :production
+set :stage, :staging
 set :port, 00000
 
 server "#{fetch(:user)}.wp.pixels.fi:#{fetch(:port)}", user: fetch(:user), roles: %w{web app db}
 
-fetch(:default_env).merge!(wp_env: :production)
+fetch(:default_env).merge!(wp_env: :staging)
