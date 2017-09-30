0a95d3814321f6327abb775cf24f4b9e3fa4b434
Thomas Hurd <thomas@pixels.fi>
2017-09-30 16:54:30 +0300

    remove filename script from pixels toolbelt and add clean filenames to composer

--- web/app/mu-plugins/pixels-toolbelt/pixels-toolbelt.php
+++ web/app/mu-plugins/pixels-toolbelt/pixels-toolbelt.php
@@ -33,32 +33,3 @@ function move_yoast_to_bottom() {
 }
 
 add_filter( 'wpseo_metabox_prio', __NAMESPACE__ . '\\move_yoast_to_bottom');
-
-/**
- * Produces cleaner filenames for uploads
- *
- * @param  string $filename
- * @return string
- */
-function wpartisan_sanitize_file_name( $filename ) {
-
-    $sanitized_filename = remove_accents( $filename ); // Convert to ASCII
-
-    // Standard replacements
-    $invalid = array(
-        ' '   => '-',
-        '%20' => '-',
-        '_'   => '-',
-    );
-    $sanitized_filename = str_replace( array_keys( $invalid ), array_values( $invalid ), $sanitized_filename );
-
-    $sanitized_filename = preg_replace('/[^A-Za-z0-9-\. ]/', '', $sanitized_filename); // Remove all non-alphanumeric except .
-    $sanitized_filename = preg_replace('/\.(?=.*\.)/', '', $sanitized_filename); // Remove all but last .
-    $sanitized_filename = preg_replace('/-+/', '-', $sanitized_filename); // Replace any more than one - in a row
-    $sanitized_filename = str_replace('-.', '.', $sanitized_filename); // Remove last - if at the end
-    $sanitized_filename = strtolower( $sanitized_filename ); // Lowercase
-
-    return $sanitized_filename;
-}
-
-add_filter( 'sanitize_file_name', 'wpartisan_sanitize_file_name', 10, 1 );
