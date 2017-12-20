71f3fcdd64fe1b291b87d030b22c6668fe6d9af8
Jeremy Desvaux <jeremy.desvaux@wonderful.fr>
2017-12-20 10:21:24 +0100

    doc

--- CI/build-documentation.sh
+++ CI/build-documentation.sh
@@ -4,17 +4,20 @@
 
 echo "\n\n//==[ Build Step 2 : Build Documentation ]==========================\n";
 
-cd ../;
-
 echo "\n=> Generating PHPDOC";
-vendor/apigen/apigen/bin/apigen generate
+php -d memory_limit=1024M vendor/bin/apigen generate vendor/wonderwp/framework/src vendor/pimple/pimple web/wp/wp-admin/includes web/wp/wp-includes --destination web/documentation/phpdoc
 
 echo "\n\n=> Generating Developer Documentation";
 echo "\n- Gathering Documentation Parts";
 rm -Rf web/documentation/dev/src/wonderwp_framework
 cp -R vendor/wonderwp/framework/documentation/ web/documentation/dev/src/wonderwp_framework
 
-rm -Rf web/documentation/dev/src/wonderwp_theme
-cp -R web/app/themes/wonderwp_theme/documentation web/documentation/dev/src/wonderwp_theme
+rm -Rf web/documentation/dev/src/wonderwp-theme-core
+cp -R web/app/themes/wonderwp-theme-core/documentation web/documentation/dev/src/wonderwp-theme-core
+
+rm -Rf web/documentation/dev/src/wonderwp-plugin-core
+cp -R web/app/mu-plugins/wonderwp-plugin-core/documentation web/documentation/dev/src/wonderwp-plugin-core
+
 echo "\n- Serving doc";
-vendor/jeremy-wdf/daux.io/bin/daux generate -s web/documentation/dev/src/ -d  web/documentation/dev/dest/ --delete
+pwd
+vendor/bin/daux generate --source=web/documentation/dev/src/ --destination=web/documentation/dev/dest/ --delete
