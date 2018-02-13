3687ff8b74a6f0bb6893fc93e8916fbac616927b
mathieu.delafosse <mathieu.delafosse@wonderful.fr>
2018-02-13 19:03:05 +0100

    Clean Styleguide

---
+++ web/app/themes/wwp_child_theme/styleguide/components/components/slider.php
@@ -0,0 +1,15 @@
+<!--components/components/slider.php-->
+
+<!--The following markup is generated using this shortcode :
+
+[slider]
+    [slider-item title="title 1" link="http://www.google.com"]
+    [slider-item title="title 2" link="http://www.google.com"]
+[/slider]
+-->
+
+<?php
+
+
+
+echo do_shortcode('[slider][slider-item title="title 1" link="http://www.google.com"][slider-item title="title 2" link="http://www.google.com"][/slider]');
