488343cb530f101a67209adcb6c3b6730f3c6297
Lafay Marc <marc.lafay@wonderful.fr>
2018-02-14 16:15:34 +0100

    Ajout styles menu bas + burger + emplacement logo

--- web/app/themes/wwp_child_theme/header.php
+++ web/app/themes/wwp_child_theme/header.php
@@ -9,53 +9,79 @@
  */
 
 ?><!DOCTYPE html>
-<html <?php language_attributes(); ?> class="no-js">
+<html <?php language_attributes (); ?> class="no-js">
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width">
+  <meta charset="<?php bloginfo ('charset'); ?>">
+  <meta name="viewport" content="width=device-width">
 
-    <title><?php
-        /*
-         * Print the <title> tag based on what is being viewed.
-         */
-        global $page, $paged;
+  <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png">
+  <link rel="manifest" href="/favicons/manifest.json">
+  <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5">
+  <link rel="shortcut icon" href="/favicons/favicon.ico">
+  <meta name="msapplication-config" content="/favicons/browserconfig.xml">
+  <meta name="theme-color" content="#ffffff">
 
-        echo stripslashes(wp_title('|', false, 'right'));
+  <title><?php
+      /*
+       * Print the <title> tag based on what is being viewed.
+       */
+      global $page, $paged;
 
-        // Add the blog name.
-        echo get_bloginfo('name');
+      echo stripslashes (wp_title ('|', false, 'right'));
 
-        // Add the blog description for the home/front page.
-        if (is_home() || is_front_page()) {
-            $site_description = get_bloginfo('description', 'display');
-            if ($site_description) {
-                echo " | $site_description";
-            }
-        }
+      // Add the blog name.
+      echo get_bloginfo ('name');
 
-        // Add a page number if necessary:
-        if ($paged >= 2 || $page >= 2) {
-            echo ' | ' . sprintf(__('Page %s', 'twentyten'), max($paged, $page));
-        }
+      // Add the blog description for the home/front page.
+      if (is_home () || is_front_page ()) {
+          $site_description = get_bloginfo ('description', 'display');
+          if ($site_description) {
+              echo " | $site_description";
+          }
+      }
 
-        ?></title>
+      // Add a page number if necessary:
+      if ($paged >= 2 || $page >= 2) {
+          echo ' | ' . sprintf (__ ('Page %s', 'twentyten'), max ($paged, $page));
+      }
 
-    <?php wp_head(); ?>
+      ?></title>
+
+    <?php wp_head (); ?>
 </head>
 
-<body <?php body_class(); ?>>
+<body <?php body_class (); ?>>
 <div id="page" class="hfeed site">
-    <div class="skip-links"><a href="#content"><?php _e('Skip to content', 'wonderwp'); ?></a></div>
+  <div class="skip-links"><a href="#content"><?php _e ('Skip to content', 'wonderwp'); ?></a></div>
 
-    <header id="header">
+  <header id="header">
+
+    <div class="inner-header">
+
+      <button class="wdf-burger nav-button" type="button" role="button" aria-label="open/close navigation"><i></i></button>
+
+        <?php
+        echo '<a href="/" class="logo">
+                <!--<img src="/app/themes/wwp_child_theme/assets/raw/svg/logo.svg" alt="Mon site - accueil">--> LOGO
+              </a>'
+        ?>
+
+      <div class="navigation-wrapper">
+
         <ul id="menu">
             <?php
             /** @var \WonderWp\Theme\Core\Service\ThemeViewService $viewService */
-            $viewService = wwp_get_theme_service('view');
-            $exclude     = [];
-            echo $viewService->getMainMenu($exclude);
+            $viewService = wwp_get_theme_service ('view');
+            $exclude = [];
+            echo $viewService->getMainMenu ($exclude);
             ?>
         </ul>
-    </header>
 
-    <div id="content" class="site-content">
+      </div>
+
+    </div>
+  </header>
+
+  <div id="content" class="site-content">
