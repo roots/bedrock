aeb4a39c7583d96be3e1c7d38019758e39b23ed7
Jeremy Desvaux <jeremy.desvaux@wonderful.fr>
2018-02-27 16:25:34 +0100

    removed old CI scripts

--- CI/code-migration.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "\n\n//==[ Build Step 3 : Code Migration ]==========================\n";
-
-echo "Hello this is build $BUILD_NUMBER";
-echo "Built into the workspace $WORKSPACE";
-echo "This build will now be sent to the $ENV environmnent";
-echo "rsync -uvrn --delete --exclude-from $WORKSPACE/CI/exclude-file.txt $WORKSPACE/* $SSHUSERNAME@$SSHSERVER:$CHEMINDIST";
-rsync -uvr --delete --exclude-from $WORKSPACE/CI/exclude-file.txt $WORKSPACE/* $SSHUSERNAME@$SSHSERVER:$CHEMINDIST;
