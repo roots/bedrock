f3b860e4d67573021d222be2d64132454f94e1e7
mathieu.delafosse <mathieu.delafosse@wonderful.fr>
2018-03-26 12:14:06 +0200

    Added information about postcss configuration

--- postcss.config.js
+++ postcss.config.js
@@ -5,3 +5,9 @@ module.exports = {
         }
     }
 }
+
+/*
+* See https://github.com/postcss/autoprefixer
+* and https://github.com/browserslist/browserslist#queries
+* for finer browser autoprefixer configuration.
+* */
