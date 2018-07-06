ae299816da1ce8fae0ac2532e2cd465c2dfac786
Lafay Marc <marc.lafay@wonderful.fr>
2018-07-06 14:28:47 +0200

    fix html tags bugs

--- web/app/themes/wwp_child_theme/header.php
+++ web/app/themes/wwp_child_theme/header.php
@@ -14,14 +14,14 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width">
 
-    <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
+    <!-- <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png">
     <link rel="manifest" href="/favicons/manifest.json">
     <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5">
     <link rel="shortcut icon" href="/favicons/favicon.ico">
     <meta name="msapplication-config" content="/favicons/browserconfig.xml">
-    <meta name="theme-color" content="#ffffff">
+    <meta name="theme-color" content="#ffffff">--> //TODO : Generate favicons here : https://realfavicongenerator.net
 
     <title><?php
         /*
