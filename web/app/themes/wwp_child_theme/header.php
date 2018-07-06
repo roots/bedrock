2441234e710581b438363365db3e19af8aa6d852
Lafay Marc <marc.lafay@wonderful.fr>
2018-07-06 14:32:59 +0200

    Favicons header tags comments

--- web/app/themes/wwp_child_theme/header.php
+++ web/app/themes/wwp_child_theme/header.php
@@ -21,7 +21,7 @@
     <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5">
     <link rel="shortcut icon" href="/favicons/favicon.ico">
     <meta name="msapplication-config" content="/favicons/browserconfig.xml">
-    <meta name="theme-color" content="#ffffff">--> //TODO : Generate favicons here : https://realfavicongenerator.net
+    <meta name="theme-color" content="#ffffff">--> <!--TODO : Generate favicons here : https://realfavicongenerator.net-->
 
     <title><?php
         /*
