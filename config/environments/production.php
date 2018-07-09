a0c795a9f936020f9d0a5cbd7f15ecd8d24212ae
Stefan Lindberg <stefan@wonderboys.se>
2018-07-09 13:45:31 +0200

    Set new default WP_POST_REVISIONS for production environment

--- config/environments/production.php
+++ config/environments/production.php
@@ -5,3 +5,5 @@ define('WP_DEBUG_DISPLAY', false);
 define('SCRIPT_DEBUG', false);
 /** Disable all file modifications including updates and update notifications */
 define('DISALLOW_FILE_MODS', true);
+/** Limit number of revisions saved */
+define('WP_POST_REVISIONS', 10);
