1e3eff7580d8e043ae070e760b77c49ca29c445f
Evan Mattson <me@aaemnnost.tv>
2018-07-10 23:44:43 +0200

    add bootstrap.php and wp-config.php

---
+++ tests/bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * PHPUnit bootstrap file
+ *
+ * Composer autoloader must be loaded before WP_PHPUNIT__DIR will be available.
+ */
+
+require_once(dirname(__DIR__) . '/vendor/autoload.php');
+
+// Give access to tests_add_filter() function.
+require_once(getenv('WP_PHPUNIT__DIR') . '/includes/functions.php');
+
+tests_add_filter('muplugins_loaded', function () {
+    // test set up, plugin activation, etc.
+});
+
+// Start up the WP testing environment.
+require(getenv('WP_PHPUNIT__DIR') . '/includes/bootstrap.php');
