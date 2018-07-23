3a505425dfcfacf7da108a6a9e5bd49d24355bfb
Jeremy Desvaux <jeremy.desvaux@wonderful.fr>
2018-07-23 17:40:26 +0200

    fix cookie component

--- web/app/themes/wwp_child_theme/assets/raw/js/app_styleguide.js
+++ web/app/themes/wwp_child_theme/assets/raw/js/app_styleguide.js
@@ -1,3 +1,4 @@
 import * as components from "../../../includes/Components/components";
 import {FormManager} from "../../../styleguide/js/components/form/form-manager";
+import {CookieComponent} from "../../../styleguide/js/components/cookies/cookieComponent";
 import {headerComponent} from "./components/headerComponent";
