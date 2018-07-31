3649719e48bb2f0882c8f46794e26877e134e2e1
QWp6t <QWp6t@users.noreply.github.com>
2018-07-31 12:41:33 -0700

    Show admin notice when indexing is discouraged

--- web/app/mu-plugins/disallow-indexing.php
+++ web/app/mu-plugins/disallow-indexing.php
@@ -3,12 +3,21 @@
 Plugin Name:  Disallow Indexing
 Plugin URI:   https://roots.io/bedrock/
 Description:  Disallow indexing of your site on non-production environments.
-Version:      1.0.0
+Version:      2.0.0
 Author:       Roots
 Author URI:   https://roots.io/
 License:      MIT License
 */
 
-if (defined('WP_ENV') && WP_ENV !== 'production' && !is_admin()) {
+if (!defined('WP_ENV') || WP_ENV === 'production') {
+    return;
+}
+
+if (!is_admin()) {
     add_action('pre_option_blog_public', '__return_zero');
+} else {
+    add_action('admin_notices', function () {
+        $message = __('Search engine indexing is currently discouraged.', 'roots');
+        echo sprintf('<div class="error"><p>%s</p></div>', $message);
+    });
 }
