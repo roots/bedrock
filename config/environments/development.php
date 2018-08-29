c457082cf4b153400d3e34f4f68a30eea4cc7c38
Michael W. Delaney <michael@michaeldelaney.me>
2018-08-29 15:37:53 -0400

    Fix error display in development environments

--- config/environments/development.php
+++ config/environments/development.php
@@ -7,6 +7,7 @@ use Roots\WPConfig\Config;
 
 Config::define('SAVEQUERIES', true);
 Config::define('WP_DEBUG', true);
+Config::define('WP_DEBUG_DISPLAY', true);
 Config::define('SCRIPT_DEBUG', true);
 
 ini_set('display_errors', 1);
