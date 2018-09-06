f50c852fc2f9f63b237871b0a48e8e872d22fcdd
Logan Stellway <loganstellway@Logans-MacBook-Pro.local>
2018-09-06 12:36:50 -0700

    Allow defining WP_CONTENT_URL in .env file
    This is helpful when creating a cookie-free domain to serve static content

--- config/application.php
+++ config/application.php
@@ -47,7 +47,7 @@ Config::define('WP_SITEURL', env('WP_SITEURL'));
  */
 Config::define('CONTENT_DIR', '/app');
 Config::define('WP_CONTENT_DIR', $webroot_dir . Config::get('CONTENT_DIR'));
-Config::define('WP_CONTENT_URL', Config::get('WP_HOME') . Config::get('CONTENT_DIR'));
+Config::define('WP_CONTENT_URL', env('WP_CONTENT_URL') ?: Config::get('WP_HOME') . Config::get('CONTENT_DIR'));
 
 /**
  * DB settings
