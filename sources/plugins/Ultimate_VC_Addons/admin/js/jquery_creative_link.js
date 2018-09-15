8a7856c6931e3a43609abea9ebfb4e9edfd55b22
Youssef BOUAZIZ <bouaziz1991@gmail.com>
2018-09-15 16:52:38 +0000

    Plugins & Theme

--- sources/plugins/Ultimate_VC_Addons/admin/js/jquery_creative_link.js
+++
@@ -1,1 +0,0 @@
-jQuery(document).ready(function(){jQuery("body").on("click",".ui-tabs-anchor",function(){var c=jQuery(this).closest("div.vc_row ").data("title");"Edit Creative Link"==c&&(jQuery(".vc_col-xs-5").children().not(".vc_checkbox").css("display","none"),jQuery(".vc_border").children().not(".vc_padding").css("display","none"),jQuery(".vc_padding").css("margin","0px"))})});
--- sources/plugins/Ultimate_VC_Addons/assets/js/vc-fronteditor.js
+++
@@ -1,1 +0,0 @@
-(function($) {	"use strict";	$(window).load(function(){		jQuery(".smile_icon_list").each(function(index, element) {            var style = jQuery(this).data('style');			var font = jQuery(this).data('fonts');			jQuery(this).find(".icon_list_icon").attr('style',style);			jQuery(this).find(".icon_list_item").attr('style',font);        });	});})(jQuery);
