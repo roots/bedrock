18a48f8b023b49de91bb8fbcd8db25c578c46282
Youssef BOUAZIZ <bouaziz1991@gmail.com>
2018-09-25 14:09:55 +0000

    Plugin install

--- web/app/themes/academy-theme-child/north-child/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-//
-// Your code goes below!
-//
-
-add_action( 'wp_enqueue_scripts', 'vntd_north_child_styles' );
-function vntd_north_child_styles() {
-    wp_enqueue_style( 'vntd-child-style', get_stylesheet_uri(), array( 'theme-styles' ) );
-}
