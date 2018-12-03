155eafe4e5d12131058279b37c79f056e53cdca3
Jeremy Desvaux <jeremy.desvaux@wonderful.fr>
2018-12-03 19:02:27 +0100

    fix styleguide footer

--- web/app/themes/wwp_child_theme/styleguide/index.php
+++ web/app/themes/wwp_child_theme/styleguide/index.php
@@ -1,8 +1,9 @@
 <?php
 if(!defined('WP_USE_THEMES')){ define('WP_USE_THEMES', false); }
 require_once( $_SERVER['DOCUMENT_ROOT'] . '/wp/wp-blog-header.php' );
-get_template_part('header'); ?>
 
-<iframe id="styleguideWrap" src="<?php echo get_stylesheet_directory_uri(); ?>/styleguide/atomic-core/?cat=base" width="100%" height="100%" style="min-height: 1024px;"></iframe>
+get_header();  ?>
 
-<?php get_template_part('footer');
+    <iframe id="styleguideWrap" src="<?php echo get_stylesheet_directory_uri(); ?>/styleguide/atomic-core/?cat=base" width="100%" height="100%" style="min-height: 1024px;"></iframe>
+
+<?php get_footer();
