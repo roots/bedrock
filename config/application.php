9eb0f9e3fdbab991a0ccaf5c054abe48a9cc9881
Nathan Knowler <nathan@knowler.me>
2019-01-28 11:31:19 -0700

    Update vlucas/phpdotenv to ^3
    Resolves #411

--- config/application.php
+++ config/application.php
@@ -24,7 +24,7 @@ Env::init();
 /**
  * Use Dotenv to set required environment variables and load .env file in root
  */
-$dotenv = new Dotenv\Dotenv($root_dir);
+$dotenv = Dotenv\Dotenv::create($root_dir);
 if (file_exists($root_dir . '/.env')) {
     $dotenv->load();
     $dotenv->required(['DB_NAME', 'DB_USER', 'DB_PASSWORD', 'WP_HOME', 'WP_SITEURL']);
