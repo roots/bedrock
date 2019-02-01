34dee6b7a978389cf2b08b732042fa72196ce0a7
Tristan Bessoussa <tristan.bessoussa@gmail.com>
2019-02-01 16:34:12 +0100

    WordPress

--- config/application.php
+++ config/application.php
@@ -90,7 +90,7 @@ Config::define('SCRIPT_DEBUG', false);
 ini_set('display_errors', 0);
 
 /**
- * Allow Wordpress to detect HTTPS when used behind a reverse proxy or a load balancer
+ * Allow WordPress to detect HTTPS when used behind a reverse proxy or a load balancer
  * See https://codex.wordpress.org/Function_Reference/is_ssl#Notes
  */
 if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
