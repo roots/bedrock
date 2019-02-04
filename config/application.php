921978cabd0640c66f6a78dcb44770398e752ee4
Tristan Bessoussa <tristan.bessoussa@gmail.com>
2019-02-04 17:12:25 +0100

    Improve required env

--- config/application.php
+++ config/application.php
@@ -27,7 +27,10 @@ Env::init();
 $dotenv = new Dotenv\Dotenv($root_dir);
 if (file_exists($root_dir . '/.env')) {
     $dotenv->load();
-    $dotenv->required(['DB_NAME', 'DB_USER', 'DB_PASSWORD', 'WP_HOME', 'WP_SITEURL']);
+    $dotenv->required(['WP_HOME', 'WP_SITEURL']);
+    if (!env('DATABASE_URL')) {
+        $dotenv->required(['DB_NAME', 'DB_USER', 'DB_PASSWORD', 'WP_HOME', 'WP_SITEURL']);
+    }
 }
 
 /**
