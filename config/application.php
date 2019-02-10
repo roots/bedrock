616b220f0423a782de5f114760fcc09cafc1fd78
Brandon <brandon@tendency.me>
2019-02-10 05:22:39 -0600

    Remove redundant null coalesce operators

--- config/application.php
+++ config/application.php
@@ -55,9 +55,9 @@ Config::define('WP_CONTENT_URL', Config::get('WP_HOME') . Config::get('CONTENT_D
 /**
  * DB settings
  */
-Config::define('DB_NAME', env('DB_NAME') ?? null);
-Config::define('DB_USER', env('DB_USER') ?? null);
-Config::define('DB_PASSWORD', env('DB_PASSWORD') ?? null);
+Config::define('DB_NAME', env('DB_NAME'));
+Config::define('DB_USER', env('DB_USER'));
+Config::define('DB_PASSWORD', env('DB_PASSWORD'));
 Config::define('DB_HOST', env('DB_HOST') ?: 'localhost');
 Config::define('DB_CHARSET', 'utf8mb4');
 Config::define('DB_COLLATE', '');
