95dc482cec9b7589a4ebafaf667c717aae8e2651
Guillermo Azurdia <info@nopticon.com>
2019-04-12 20:40:30 -0500

    When defining WP_CONTENT_URL, if WP_HOME it's defined in config file, domain is not set

--- config/application.php
+++ config/application.php
@@ -50,7 +50,6 @@ Config::define('WP_SITEURL', env('WP_SITEURL'));
  */
 Config::define('CONTENT_DIR', '/app');
 Config::define('WP_CONTENT_DIR', $webroot_dir . Config::get('CONTENT_DIR'));
-Config::define('WP_CONTENT_URL', Config::get('WP_HOME') . Config::get('CONTENT_DIR'));
 
 /**
  * DB settings
@@ -114,6 +113,11 @@ $env_config = __DIR__ . '/environments/' . WP_ENV . '.php';
 if (file_exists($env_config)) {
     require_once $env_config;
 }
+
+/**
+ * Custom Content URL
+ */
+Config::define('WP_CONTENT_URL', Config::get('WP_HOME') . Config::get('CONTENT_DIR'));
 
 Config::apply();
 
