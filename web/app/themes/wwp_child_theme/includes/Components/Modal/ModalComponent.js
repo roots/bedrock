b82b0b47ffb076d5196d52b3f544981458a536d5
Marc Lafay <marc.lafay@wonderful.fr>
2019-04-18 16:58:21 +0200

    Fix modal component JS

--- web/app/themes/wwp_child_theme/includes/Components/Modal/ModalComponent.js
+++ web/app/themes/wwp_child_theme/includes/Components/Modal/ModalComponent.js
@@ -3,16 +3,23 @@ import {PewComponent} from "../../../assets/raw/js/components/pew-component";
 
 // https://github.com/humaan/Modaal
 const ModalOptions = {
-
+    background: "#E4E4E4",
+    overlay_opacity: .9,
+    fullscreen: false
 };
 
 export class ModalComponent extends PewComponent {
     constructor(element) {
+        if ($(element).data('modaal-type')) {
+            ModalOptions['type'] = $(element).data('modaal-type');
+        }
         super(element, ModalOptions);
     }
+
     init() {
         this.element.modaal(this.options);
     }
 }
 
-window.pew.addRegistryEntry({key: 'wdf-modal', domSelector: '.wdf-modal', classDef: ModalComponent});
+// Default
+window.pew.addRegistryEntry({key: "wdf-modal", domSelector: ".wdf-modal", classDef: ModalComponent});
