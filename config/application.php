9a3d1bd92b06f3d4ab642024b72816c37d13e740
Dominic Rubas <rubas@users.noreply.github.com>
2019-08-14 21:27:07 +0200

    ini_set() expects parameter 2 to be string
    Uncaught TypeError: ini_set() expects parameter 2 to be string, int given

--- config/application.php
+++ config/application.php
@@ -99,7 +99,7 @@ Config::define('DISALLOW_FILE_MODS', true);
  */
 Config::define('WP_DEBUG_DISPLAY', false);
 Config::define('SCRIPT_DEBUG', false);
-ini_set('display_errors', 0);
+ini_set('display_errors', '0');
 
 /**
  * Allow WordPress to detect HTTPS when used behind a reverse proxy or a load balancer
