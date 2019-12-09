0e114716d27c4fef38ba8995e6f6755ebba6d902
Kelly Mears <developers@tinypixel.dev>
2019-12-09 09:49:06 -0800

    Upgrades vlucas/phpdotenv to ^4.0.1 and implements new initialization method

--- config/application.php
+++ config/application.php
@@ -24,7 +24,7 @@ Env::init();
 /**
  * Use Dotenv to set required environment variables and load .env file in root
  */
-$dotenv = Dotenv\Dotenv::create($root_dir);
+$dotenv = Dotenv\Dotenv::createMutable($root_dir);
 if (file_exists($root_dir . '/.env')) {
     $dotenv->load();
     $dotenv->required(['WP_HOME', 'WP_SITEURL']);
