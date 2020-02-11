d0dcc2d1c59c4d54e5eeeb627e37e9e6e6a86ab3
Marc Lafay <marc.lafay@wonderful.fr>
2020-02-11 10:31:34 +0100

    [card] Ajout de classes utilitaires pour préparer options de Gutenberg

--- web/app/themes/wwp_child_theme/styleguide/components/molecules/card.php
+++ web/app/themes/wwp_child_theme/styleguide/components/molecules/card.php
@@ -1,31 +1,86 @@
 <!-- components/molecules/card.php -->
-<p class="subTitle">Bloc de contenu polyvalent.</p>
-<div class="grid-2 has-gutter-xl">
-    <div>
-        <p>ATTRIBUTS :</p>
-        <ul>
-            <li>title</li>
-            <li>text</li>
-            <li>img</li>
-            <li>link</li>
-            <li>button</li>
-            <li>color</li>
-            <li>background</li>
-            <li>class</li>
-        </ul>
-    </div>
-    <div>
-        <p>OPTIONS :</p>
-        <ul>
-            <li>reverse</li>
-            <li>two-cols</li>
-            <li>inline</li>
-        </ul>
+<p class="subTitle">Bloc de contenu polyvalent</p>
+
+<div class="encadre">
+    <div class="grid-2 has-gutter-xl">
+        <div>
+            <p>ATTRIBUTS :</p>
+            <ul>
+                <li>title</li>
+                <li>text</li>
+                <li>img</li>
+                <li>link</li>
+                <li>button</li>
+                <li>color</li>
+                <li>background</li>
+                <li>class</li>
+            </ul>
+        </div>
+        <div>
+            <p>OPTIONS :</p>
+            <ul>
+                <li>reverse</li>
+                <li>landscape</li>
+            </ul>
+        </div>
     </div>
 </div>
 
-<div class="grid-3-small-2 has-gutter-xl">
-    <?php echo do_shortcode ('[card title="Lorem ipsum dolor sit amet" content="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam." image="https://via.placeholder.com/150" link="/une-page" button="Découvrir"]'); ?>
-    <?php echo do_shortcode ('[card title="Lorem ipsum" content="Lorem ipsum dolor sit amet, consectetur adipisicing elit." image="https://via.placeholder.com/150" link="/une-page" button="Découvrir" class="negative"]'); ?>
-    <?php echo do_shortcode ('[card title="Lorem ipsum dolor sit amet" content="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam." image="https://via.placeholder.com/150" link="/une-page" button="Découvrir" class="reverse"]'); ?>
+<div class="grid-2 has-gutter-l">
+
+    <?php
+
+    echo '<div>
+        <p class="subTitle">Sans option</p>';
+        $card1 = new \WonderWp\Theme\Child\Components\Card\CardComponent();
+        $card1
+            ->setTitle('Lorem ipsum dolor consectetur')
+            ->setContent('Lorem ipsum dolor, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.')
+            ->setImage('<img src="https://via.placeholder.com/150" alt="">')
+            ->setLink('/vers-une-page')
+            ->setDate('12 septembre 2020')
+            /*->setCategories('')*/
+        ;
+        echo $card1->getMarkup();
+    echo '</div>';
+    echo '<div class="reverse-inside">
+        <p class="subTitle">Avec option "reverse"</p>';
+        $card2 = new \WonderWp\Theme\Child\Components\Card\CardComponent();
+        $card2
+            ->setTitle('Lorem ipsum dolor consectetur')
+            ->setContent('Lorem ipsum dolor, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.')
+            ->setImage('<img src="https://via.placeholder.com/150" alt="">')
+            ->setLink('/vers-une-page')
+            ->setDate('12 septembre 2020')
+            /*->setCategories('')*/
+        ;
+        echo $card2->getMarkup();
+    echo '</div>';
+    echo '<div class="col-2 landscape-inside">
+        <p class="subTitle">Avec option "landscape"</p>';
+        $card3 = new \WonderWp\Theme\Child\Components\Card\CardComponent();
+        $card3
+            ->setTitle('Lorem ipsum dolor consectetur')
+            ->setContent('Lorem ipsum dolor, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.')
+            ->setImage('<img src="https://via.placeholder.com/150" alt="">')
+            ->setLink('/vers-une-page')
+            ->setDate('12 septembre 2020')
+            /*->setCategories('')*/
+        ;
+        echo $card3->getMarkup();
+    echo '</div>';
+    echo '<div class="col-2 landscape-inside reverse-inside">
+        <p class="subTitle">Avec options "landscape" et "reverse"</p>';
+        $card4 = new \WonderWp\Theme\Child\Components\Card\CardComponent();
+        $card4
+            ->setTitle('Lorem ipsum dolor consectetur')
+            ->setContent('Lorem ipsum dolor, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.')
+            ->setImage('<img src="https://via.placeholder.com/150" alt="">')
+            ->setLink('/vers-une-page')
+            ->setDate('12 septembre 2020')
+            /*->setCategories('')*/
+        ;
+        echo $card4->getMarkup();
+    echo '</div>';
+    ?>
 </div>
