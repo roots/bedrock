675d671a0473865613d31944ef0378d751203c85
Jeremy Desvaux <jeremy.desvaux@wonderful.fr>
2020-02-11 12:35:13 +0100

    [js] ie compatible bootsrap

--- web/app/themes/wwp_child_theme/assets/raw/js/app_init.js
+++ web/app/themes/wwp_child_theme/assets/raw/js/app_init.js
@@ -1,1 +1,19 @@
-if(window.pew) { window.pew.enhanceRegistry(); }
+const svg4Everybody = require('svg4everybody');
+
+class App {
+  init() {
+    this.launchPew();
+    this.launchSvg4Everybody();
+  }
+
+  launchPew() {
+    if (window.pew) { window.pew.enhanceRegistry(document); }
+  }
+
+  launchSvg4Everybody() {
+    svg4Everybody();
+  }
+}
+
+let app = new App();
+app.init();
