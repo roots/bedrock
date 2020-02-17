ee64d77050cd0082a4304da0950d2ce46ef68761
Marc Lafay <marc.lafay@wonderful.fr>
2020-02-17 18:42:30 +0100

    [styleguide] Correction orthographe

--- web/app/themes/wwp_child_theme/styleguide/components/base/images.php
+++ web/app/themes/wwp_child_theme/styleguide/components/base/images.php
@@ -1,9 +1,9 @@
 <!-- components/base/images.php -->
 
-<p class="subtitle">Image à la une détail Actualités : 1730x530px</p>
+<p class="subtitle">Image à la une détail actualité : 1730x530px</p>
 <img width="1730" height="530" src="https://via.placeholder.com/1730x530" alt="">
 <hr>
 
-<p class="subtitle">Image thumbnail lite actualités : 298x450px</p>
+<p class="subtitle">Image thumbnail liste actualités : 298x450px</p>
 <img width="298" height="450" src="https://via.placeholder.com/298x450" alt="">
 <hr>
