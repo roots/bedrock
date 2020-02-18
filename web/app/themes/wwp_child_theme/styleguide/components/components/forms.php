6a2c5d66c04fad49c976db0f9f260bce6efac71b
Marc Lafay <marc.lafay@wonderful.fr>
2020-02-18 13:42:49 +0100

    [forms] Ajout options : inline et sans label

--- web/app/themes/wwp_child_theme/styleguide/components/components/forms.php
+++ web/app/themes/wwp_child_theme/styleguide/components/components/forms.php
@@ -1,3 +1,22 @@
 <!-- components/components/forms.php -->
-
+<p class="subTitle">Formulaire de base</p>
 <?php echo do_shortcode("[wwpmodule slug='wwp-contact' form='1']"); ?>
+<br>
+
+<p class="subTitle">Formulaire labels masqués</p>
+<div class="form-nolabel">
+    <?php echo do_shortcode("[wwpmodule slug='wwp-contact' form='1']"); ?>
+</div>
+<br>
+
+<p class="subTitle">Formulaire horizontal (inline)</p>
+<div class="form-inline">
+    <?php echo do_shortcode("[wwpmodule slug='wwp-contact' form='1']"); ?>
+</div>
+<br>
+
+<p class="subTitle">Formulaire inline avec labels masqués</p>
+<div class="form-nolabel form-inline">
+    <?php echo do_shortcode("[wwpmodule slug='wwp-contact' form='1']"); ?>
+</div>
+
