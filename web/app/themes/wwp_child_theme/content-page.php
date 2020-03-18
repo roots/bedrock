7fa4823bea98a4cfe2a5a7490236cf4ece757f8a
Jeremy Desvaux <jeremy.desvaux@wonderful.fr>
2020-03-18 19:13:17 +0100

    suppression de wp_link_pages

--- web/app/themes/wwp_child_theme/content-page.php
+++ web/app/themes/wwp_child_theme/content-page.php
@@ -34,16 +34,7 @@
         } ?>
 
         <?php the_content(); ?>
-        <?php
-        wp_link_pages([
-            'before'      => '<div class="page-links"><span class="page-links-title">' . __('Pages:', 'twentyfifteen') . '</span>',
-            'after'       => '</div>',
-            'link_before' => '<span>',
-            'link_after'  => '</span>',
-            'pagelink'    => '<span class="screen-reader-text">' . __('Page', 'twentyfifteen') . ' </span>%',
-            'separator'   => '<span class="screen-reader-text">, </span>',
-        ]);
-        ?>
+
         </main><!-- main -->
     </div><!-- .entry-content -->
 
