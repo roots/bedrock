c02971644f5b1715ba1c839d385d0e90cd0a8006
Jeremy Desvaux <jeremy.desvaux@wonderful.fr>
2020-03-19 14:07:26 +0100

    [slider] maj régléges par défaut du slider

--- web/app/themes/wwp_child_theme/includes/Components/Slider/SliderComponent.js
+++ web/app/themes/wwp_child_theme/includes/Components/Slider/SliderComponent.js
@@ -3,24 +3,37 @@ import {PewComponent} from "../../../assets/raw/js/components/pew-component";
 
 // see https://bxslider.com/options/
 const SliderOptions = {
-    mode: 'horizontal',
-    autoControlsCombine: true,
-    keyboardEnabled: true,
-    easing: 'ease-in-out',
-    speed: 700,
-    auto: true,
-    autoControls: false,
-    pager: true,
-    stopAutoOnClick: true
+  mode: 'horizontal',
+  autoControlsCombine: true,
+  keyboardEnabled: true,
+  easing: 'ease-in-out',
+  speed: 700,
+  auto: true,
+  autoControls: false,
+  pager: true,
+  stopAutoOnClick: true,
+  adaptiveHeight: true,
+  touchEnabled: (window.innerWidth<1025 && window.wonderwp.FeatureDetector.has('touch'))
 };
 
 export class SliderComponent extends PewComponent {
-    constructor(element) {
-        super(element, SliderOptions);
-    }
-    init() {
-        this.element.bxSlider(this.options);
-    }
+  constructor(element) {
+    super(element, SliderOptions);
+  }
+
+  init() {
+    this.slider = this.element.bxSlider(this.options);
+    $(document).on('ready', () => {
+      this.reloadSlider('domready');
+    });
+    setTimeout(() => {
+      this.reloadSlider('timeout');
+    }, 800);
+  }
+
+  reloadSlider(origin) {
+    this.slider.reloadSlider(this.options);
+  }
 }
 
 window.pew.addRegistryEntry({key: 'wdf-slider-home', domSelector: '.wdf-slider', classDef: SliderComponent});
