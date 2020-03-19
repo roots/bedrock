511aead3d836fb8360afb8d4eb3aa16e58cd216e
Jeremy Desvaux <jeremy.desvaux@wonderful.fr>
2020-03-19 15:28:21 +0100

    [dropdown] code commenté pr dropdown au hover

--- web/app/themes/wwp_child_theme/includes/Components/Dropdown/DropdownComponent.js
+++ web/app/themes/wwp_child_theme/includes/Components/Dropdown/DropdownComponent.js
@@ -10,6 +10,40 @@ export class DropdownComponent extends PewComponent {
             e.preventDefault();
             $(this).parent().toggleClass('open')
         });
+
+
+      /*
+      //Code si on veut du hover à la place du click :
+
+      if (wonderwp.FeatureDetector.has('touch')) {
+
+        this.element.find('.dropdown-hover-trigger').on('click', function (e) {
+
+          let $openedElt = $('.wdf-dropdown.open');
+          let $parentElt = $(this).parents('.wdf-dropdown');
+          if ($openedElt.index('.wdf-dropdown') === $parentElt.index('.wdf-dropdown')) {
+            $parentElt.toggleClass('open');
+          } else {
+            $openedElt.removeClass('open');
+            $parentElt.addClass('open');
+          }
+
+          $(document).click(function (event) {
+            let $target = $(event.target);
+            if (!$target.closest('.wdf-dropdown.open').length && $('.wdf-dropdown.open').length) {
+              $('.wdf-dropdown.open').removeClass('open');
+            }
+          });
+
+        });
+
+      } else {
+        this.element.find('.dropdown-hover-trigger').hover(function (e) {
+          $(this).parents('.wdf-dropdown').addClass('open')
+        }, function () {
+          $(this).parents('.wdf-dropdown').removeClass('open')
+        });
+      } */
     }
 }
 
