bb426e3e7e3c32b7ba54c469b446e12dcb9890aa
marmorag <guillaume.marmorat@yproximite.com>
2020-04-28 11:18:04 +0200

    remove no more useful init call

--- config/application.php
+++ config/application.php
@@ -25,11 +25,6 @@ $root_dir = dirname(__DIR__);
 $webroot_dir = $root_dir . '/web';
 
 /**
- * Expose global env() function from oscarotero/env
- */
-Env::init();
-
-/**
  * Use Dotenv to set required environment variables and load .env file in root
  */
 $dotenv = Dotenv\Dotenv::createImmutable($root_dir);
