ef6ec533e455c70a61941f7aecb3fe17acaf899f
Jeremy Desvaux <jeremy.desvaux@wonderful.fr>
2020-04-29 12:07:22 +0200

    [cypress] reverted test to old login syntax + maj credentials

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -10,25 +10,24 @@
 //
 //
 // -- This is a parent command --
-Cypress.Commands.add("wpLogin", (email, password, callback) => {
-  let host = Cypress.env('host') || Cypress.config('host'),
-    conf = Cypress.config('wp-admin');
-  /*cy.visit(host + conf.url);
-  cy.get('#user_login').type(conf.login);
-  cy.get('#user_pass').type(conf.pwd);
-  cy.get('#loginform').submit();*/
-  cy.request({
-    method: 'POST',
-    url: host + conf.url, // baseUrl is prepended to url
-    form: true, // indicates the body should be form urlencoded and sets Content-Type: application/x-www-form-urlencoded headers
-    body: {
-      log: conf.login,
-      pwd: conf.pwd
-    }
-  }).then((response) => {
-    console.log(response);
-    callback && callback();
-  });
+Cypress.Commands.add("wpLogin", (email, password) => {
+    let host = Cypress.env('host') || Cypress.config('host'),
+        conf = Cypress.config('wp-admin');
+    /*cy.visit(host + conf.url);
+    cy.get('#user_login').type(conf.login);
+    cy.get('#user_pass').type(conf.pwd);
+    cy.get('#loginform').submit();*/
+    cy.request({
+        method: 'POST',
+        url: host+conf.url, // baseUrl is prepended to url
+        form: true, // indicates the body should be form urlencoded and sets Content-Type: application/x-www-form-urlencoded headers
+        body: {
+            log: conf.login,
+            pwd: conf.pwd
+        }
+    }).then((response) => {
+        console.log(response);
+    });
 });
 //
 //
