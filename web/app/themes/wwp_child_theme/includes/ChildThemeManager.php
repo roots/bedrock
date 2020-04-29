91c6d814172754957aae9fe5f7abf4d93172319f
Jeremy Desvaux <jeremy.desvaux@wonderful.fr>
2020-04-29 10:38:55 +0200

    [formatting] foramttage

--- web/app/themes/wwp_child_theme/includes/ChildThemeManager.php
+++ web/app/themes/wwp_child_theme/includes/ChildThemeManager.php
@@ -26,33 +26,33 @@ class ChildThemeManager extends ThemeManager
     public function register(Container $container)
     {
 
-        $this->setConfig('use_critical_css',true);
+        $this->setConfig('use_critical_css', true);
 
         parent::register($container);
 
         //Controllers
-        $this->addController(AbstractManager::PUBLIC_CONTROLLER_TYPE,function(){
-           return new ThemePublicController();
+        $this->addController(AbstractManager::PUBLIC_CONTROLLER_TYPE, function () {
+            return new ThemePublicController();
         });
 
         //Hooks
-        $this->addService(ServiceInterface::HOOK_SERVICE_NAME,function(){
+        $this->addService(ServiceInterface::HOOK_SERVICE_NAME, function () {
             return new ChildThemeHookService($this);
         });
         //Routes
-        $this->addService(ServiceInterface::ROUTE_SERVICE_NAME,function(){
+        $this->addService(ServiceInterface::ROUTE_SERVICE_NAME, function () {
             return new ThemeRouteService($this);
         });
         //Assets
-        $this->addService(ServiceInterface::ASSETS_SERVICE_NAME,function(){
+        $this->addService(ServiceInterface::ASSETS_SERVICE_NAME, function () {
             return new ThemeAssetService($this);
         });
         //Shortcodes
-        $this->addService(ServiceInterface::SHORT_CODE_SERVICE_NAME,function(){
+        $this->addService(ServiceInterface::SHORT_CODE_SERVICE_NAME, function () {
             return new ChildThemeShortcodeService($this);
         });
 
-        $container['wwp.theme.component.card'] = $container->factory(function(){
+        $container['wwp.theme.component.card']       = $container->factory(function () {
             return new CardComponent();
         });
         $container['wwp.theme.component.pagination'] = function () {
