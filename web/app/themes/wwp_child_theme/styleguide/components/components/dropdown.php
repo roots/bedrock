100eff84c1bd9273c3e64fe979f837ba87b1e434
Marc Lafay <marc.lafay@wonderful.fr>
2020-05-10 15:48:16 +0200

    [[dropdown] Ajout composant dans le styleguide

--- web/app/themes/wwp_child_theme/styleguide/components/components/dropdown.php
+++ web/app/themes/wwp_child_theme/styleguide/components/components/dropdown.php
@@ -1,4 +1,13 @@
 <!-- components/components/dropdown.php -->
-<!--web/app/themes/wwp_child_theme/includes/Components/Dropdown/dropdown-component.html.twig-->
 
-<?php echo do_shortcode('[dropdown class="dropdown"]');
+<?php
+
+$dropdown = new \WonderWp\Theme\Child\Components\Dropdown\DropdownComponent();
+
+$dropdown
+    ->setClass('custom-class')
+    ->setLabel('Survolez ce texte pour faire apparaître le contenu')
+    ->setSubComponents('<h3>Titre du contenu masqué</h3><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>')
+    ;
+
+echo $dropdown->getMarkup();
