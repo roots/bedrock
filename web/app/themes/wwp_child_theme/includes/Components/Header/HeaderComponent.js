7925ecb486589c46408d9085fad31304ef6d61f2
Jeremy Desvaux <jeremy.desvaux@wonderful.fr>
2020-05-18 12:20:43 +0200

    [header] possibility to fix header

--- web/app/themes/wwp_child_theme/includes/Components/Header/HeaderComponent.js
+++ web/app/themes/wwp_child_theme/includes/Components/Header/HeaderComponent.js
@@ -3,36 +3,69 @@ import {Menu} from "./Menu";
 
 class headerComponent extends PewComponent {
 
-    constructor(element, options) {
-        let defaultOptions = {
-            'classToToggleOnBody': 'has-opened-menu'
-        };
-        super(element, Object.assign(defaultOptions, options));
-        this.menu = null;
-    }
+  constructor(element, options) {
+    let defaultOptions = {
+      'classToToggleOnBody': 'has-opened-menu',
+      'classToCheckIfHeaderShouldBeSticky': 'stickable',
+      'classToToggleOnBodyWhenHeaderStuck': 'has-stuck-header',
+      'stickyOffset': 'bottom'
+    };
+    super(element, Object.assign(defaultOptions, options));
+    this.menu = null;
+  }
 
-    init(){
-        this.registerMenuToggler();
-        this.registerSticky();
-    }
+  init() {
+    this.registerMenuToggler();
+    this.registerSticky();
+  }
 
-    registerMenuToggler(){
-        this.element.find('[data-menu-toggler]').on('click',(e)=>{
-            e.preventDefault();
-            if (!window.matchMedia("(min-width: 767px)").matches) {
-                if(this.menu===null) {
-                    this.menu = new Menu(this.element.find('.navigation-wrapper'));
-                }
-            }
-            $(e.currentTarget).toggleClass('is-active');
-            $('body').toggleClass(this.options.classToToggleOnBody);
-            this.menu.toggle();
-        });
-    }
+  registerMenuToggler() {
+    this.element.find('[data-menu-toggler]').on('click', (e) => {
+      e.preventDefault();
+      if (!window.matchMedia("(min-width: 767px)").matches) {
+        if (this.menu === null) {
+          this.menu = new Menu(this.element.find('.navigation-wrapper'));
+        }
+      }
+      $(e.currentTarget).toggleClass('is-active');
+      $('body').toggleClass(this.options.classToToggleOnBody);
+      this.menu.toggle();
+    });
+  }
 
-    registerSticky(){
+  registerSticky() {
+    if (this.element.hasClass(this.options.classToCheckIfHeaderShouldBeSticky)) {
+      window.onscroll = function () {checkHeaderFixation()};
+      checkHeaderFixation();
+      
+      const header = this.element[0];
+      let stickTreshold = header.offsetTop;
+      if (this.options.stickyOffset === 'top') {
+        stickTreshold += 0;
+      } else if (this.options.stickyOffset === 'center') {
+        stickTreshold += this.element.outerHeight() / 2;
+      } else if (this.options.stickyOffset === 'bottom') {
+        stickTreshold += this.element.outerHeight();
+      } else {
+        stickTreshold += this.options.stickyOffset;
+      }
+      const classToToggleOnBodyWhenHeaderStuck = this.options.classToToggleOnBodyWhenHeaderStuck;
+
+      function checkHeaderFixation() {
+        let $body = $('body');
+        if (window.pageYOffset > stickTreshold) {
+          if (!$body.hasClass(classToToggleOnBodyWhenHeaderStuck)) {
+            $body.addClass(classToToggleOnBodyWhenHeaderStuck);
+          }
+        } else {
+          if ($body.hasClass(classToToggleOnBodyWhenHeaderStuck)) {
+            $body.removeClass(classToToggleOnBodyWhenHeaderStuck);
+          }
+        }
+      }
 
     }
+  }
 
 }
 
