868be75296cceb6cb9e4233875621db2111ab6de
Jeremy Desvaux <jeremy.desvaux@wonderful.fr>
2020-07-10 16:52:20 +0200

    [RS] new vimeo support

--- web/app/themes/wwp_child_theme/includes/Components/ReseauxSociaux/ReseauxSociauxComponent.php
+++ web/app/themes/wwp_child_theme/includes/Components/ReseauxSociaux/ReseauxSociauxComponent.php
@@ -15,6 +15,7 @@ class ReseauxSociauxComponent extends AbstractComponent
             'facebook',
             'twitter',
             'youtube',
+            'vimeo',
             'instagram',
             'pinterest',
             'flickr',
