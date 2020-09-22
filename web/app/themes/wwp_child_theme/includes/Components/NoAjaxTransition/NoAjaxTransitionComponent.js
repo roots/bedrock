f43e088d2df0749cc13bbdd7630cf1545d47558b
Jeremy Desvaux <jeremy.desvaux@wonderful.fr>
2020-09-22 18:22:44 +0200

    [noajax transition] smooth scroll anchored links (works with classes as well)

--- web/app/themes/wwp_child_theme/includes/Components/NoAjaxTransition/NoAjaxTransitionComponent.js
+++ web/app/themes/wwp_child_theme/includes/Components/NoAjaxTransition/NoAjaxTransitionComponent.js
@@ -25,15 +25,42 @@ export default class NoAjaxTransitionComponent extends PewComponent {
           || href.indexOf('./') !== -1 // Lien relatif
         )
         && href.indexOf('#') === -1 //Pas une ancre
-        && ( !$(e.currentTarget).attr('target') || $link.attr('target') !== '_blank') //Pas un target blank
+        && (!$(e.currentTarget).attr('target') || $link.attr('target') !== '_blank') //Pas un target blank
         && !e.metaKey //Meta key is not pressed
       ) {
         console.log('[Transition] lien concerne');
         $('#content').addClass('transitionning');
       } else {
         console.log('[Transition] lien non concerne');
+
+        if (href.indexOf('#') >= 0 && href !== '#') {
+          //this is an anchored link
+          //Clean up anchor
+          let selector = href.split('#')[1];
+          //test with id
+          let $target = $('#' + selector);
+          if ($target.length === 0) {
+            //Test witch class
+            $target = $('.' + selector);
+          }
+          if ($target.length) {
+            e.preventDefault();
+            let topPos = $target.offset().top;
+            this.scrollTo(topPos);
+          }
+        }
+
       }
     });
+  }
+
+  scrollTo(topPos) {
+    if (window.smoothScrollMargin) {
+      topPos -= window.smoothScrollMargin;
+    }
+    $('html,body').stop().animate({
+      scrollTop: topPos
+    }, 750);
   }
 
 }
