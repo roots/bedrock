5614a1133835fa29796c3a3733a4d242a884970e
Jeremy Desvaux <jeremy.desvaux@wonderful.fr>
2020-09-22 16:40:23 +0200

    [styleguide] class no-js replaced by js class on styleguide html tag

--- web/app/themes/wwp_child_theme/styleguide/atomic-foot.php
+++ web/app/themes/wwp_child_theme/styleguide/atomic-foot.php
@@ -4,7 +4,11 @@
 <script src="js/min/main.js"></script>
 <!-- Place any footer info you would like shared between the styleguide and the root of your project. Eg. Links to js scripts etc.. -->
 
-<script>window.wonderwp = window.wonderwp || {};</script>
+<script>
+    window.wonderwp = window.wonderwp || {};
+    window.document.documentElement.className += ' js-enabled';
+    window.document.documentElement.classList.remove('no-js');
+</script>
 
 <?php
 $final_path_dir = get_stylesheet_directory().'/assets/final';
