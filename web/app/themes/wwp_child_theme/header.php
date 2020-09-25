0ae3bd070e3b4a67e35b03b4aa7fc409d65550bb
Jeremy Desvaux <jeremy.desvaux@wonderful.fr>
2020-09-25 10:23:41 +0200

    [header] fix pipe in title on home

--- web/app/themes/wwp_child_theme/header.php
+++ web/app/themes/wwp_child_theme/header.php
@@ -31,10 +31,12 @@
 
         $pageTitle = stripslashes(wp_title('|', false, 'right'));
         $pageTitle = str_replace(['|'], [' '], $pageTitle);
-        echo $pageTitle;
+        if(!empty($pageTitle)) {
+            echo $pageTitle . ' | ';
+        }
 
         // Add the blog name.
-        echo ' | ' . get_bloginfo('name');
+        echo get_bloginfo('name');
 
         // Add the blog description for the home/front page.
         if (is_home() || is_front_page()) {
