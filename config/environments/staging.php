f8a31e8b582f7bfa6735a9b757d2f105f8799223
Youssef BOUAZIZ <bouaziz1991@gmail.com>
2020-09-30 15:00:11 +0000

    Staging Debug

--- config/environments/staging.php
+++ config/environments/staging.php
@@ -1,6 +1,6 @@
 <?php
 /** Staging */
-ini_set('display_errors', 0);
+ini_set('display_errors', 1);
 define('WP_DEBUG_DISPLAY', true);
 define('SCRIPT_DEBUG', true);
 /** Disable all file modifications including updates and update notifications */
