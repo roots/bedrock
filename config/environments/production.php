491f7ace0ae8c4744065b390521205333b72c348
Youssef BOUAZIZ <bouaziz1991@gmail.com>
2020-10-06 11:25:25 +0000

    Production env

--- config/environments/production.php
+++ config/environments/production.php
@@ -2,6 +2,6 @@
 /** Production */
 ini_set('display_errors', 0);
 define('WP_DEBUG_DISPLAY', true);
-define('SCRIPT_DEBUG', true);
+define('SCRIPT_DEBUG', false);
 /** Disable all file modifications including updates and update notifications */
 define('DISALLOW_FILE_MODS', true);
