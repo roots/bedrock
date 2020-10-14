ad9607a31cb7a7ebc9443e4413388f53134bb0c7
Jeremy Desvaux <jeremy.desvaux@wonderful.fr>
2020-10-14 15:16:25 +0200

    [btn] btn component moved to theme core

--- web/app/themes/wwp_child_theme/includes/Components/Button/ButtonComponent.php
+++ web/app/themes/wwp_child_theme/includes/Components/Button/ButtonComponent.php
@@ -9,64 +9,8 @@ use WonderWp\Theme\Core\Component\AbstractComponent;
 
 /**
  * @Block(title="Bouton")
- *
  */
-class ButtonComponent extends AbstractComponent
+class ButtonComponent extends \WonderWp\Theme\Core\Component\ButtonComponent
 {
-    /**
-     * @var string
-     * @BlockAttributes(component="PlainText",type="string",componentAttributes={"placeholder":"Label"})
-     */
-    protected $label;
 
-    /**
-     * @var string
-     * @BlockAttributes(component="PlainText",type="string",componentAttributes={"placeholder":"Lien"})
-     */
-    protected $link;
-
-    /**
-     * @var string
-     * @BlockAttributes(component="MediaUpload",type="string",componentAttributes={"size":"small"})
-     */
-    protected $icon;
-
-    /**
-     * @param string $label
-     * @return ButtonComponent
-     */
-    public function setLabel(string $label): ButtonComponent
-    {
-        $this->label = $label;
-        return $this;
-    }
-
-    /**
-     * @param string $link
-     * @return ButtonComponent
-     */
-    public function setLink(string $link): ButtonComponent
-    {
-        $this->link = $link;
-        return $this;
-    }
-
-    /**
-     * @param string $icon
-     * @return ButtonComponent
-     */
-    public function setIcon(string $icon): ButtonComponent
-    {
-        $this->icon = $icon;
-        return $this;
-    }
-
-    public function getMarkup(array $opts = [])
-    {
-        $markup = '<a href="">';
-
-        $markup .= '</a>';
-
-        return $markup;
-    }
 }
