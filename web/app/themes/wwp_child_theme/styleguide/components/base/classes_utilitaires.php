01ecc2add64aed040b2218c5d033e6473c89c5a5
Marc Lafay <marc.lafay@wonderful.fr>
2021-02-12 10:57:21 +0100

    [styleguide] Ajoute classes utilitaires

--- web/app/themes/wwp_child_theme/styleguide/components/base/classes_utilitaires.php
+++ web/app/themes/wwp_child_theme/styleguide/components/base/classes_utilitaires.php
@@ -1,5 +1,14 @@
 <!-- components/base/classes_utilitaires.php -->
-
+<p class="subtitle">Marges internes et externes</p>
+<p>Toutes les classes utilitaires son listées dans le fichier : <code>/web/app/themes/wwp_child_theme/assets/raw/scss/_utilities.scss</code></p>
+<p>Elles apportent des fonctionnalités au niveau de :</p>
+<ul>
+    <li>Marges internes et externes (en rem) : <code>.mtl</code></li>
+    <li>Largeurs (en pourcentage) : <code>.w50</code></li>
+    <li>Tailles de fonts (en em) : <code>.u-big</code></li>
+    <li>Orientation layout (flexbox) : <code>.flex-center</code></li>
+    <li>Gouttières de colonnes <code>.has-gutter-xxl</code></li>
+</ul>
 <?php
 $sizes      = ['s', 'm', 'l', 'xl', 'xxl'];
 $spaces     = ['m', 'p'];
@@ -27,3 +36,5 @@ foreach ($sizes as $size) {
     echo $block;
 }
 ?>
+
+<hr>
