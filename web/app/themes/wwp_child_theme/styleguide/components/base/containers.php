ee133197a27c6a9b081af5e00a36ec55988b36ed
Marc Lafay <marc.lafay@wonderful.fr>
2021-02-12 10:44:22 +0100

    [styleguide] Ajoute les containers

---
+++ web/app/themes/wwp_child_theme/styleguide/components/base/containers.php
@@ -0,0 +1,15 @@
+<!-- components/base/containers.php -->
+<p>.container-xs (385px)</p>
+<div class="container-xs"></div>
+
+<p>.container-s (768px)</p>
+<div class="container-s"></div>
+
+<p>.container (960px)</p>
+<div class="container"></div>
+
+<p>.container-l (1280px)</p>
+<div class="container-l"></div>
+
+<p>.container-xl (1440px)</p>
+<div class="container-xl"></div>
