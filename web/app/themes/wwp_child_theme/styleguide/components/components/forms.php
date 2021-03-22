5217ca73e3c7a284504b1ad836a7efaf9f600490
Marc Lafay <marc.lafay@wonderful.fr>
2021-03-22 12:50:01 +0100

    [forms] Ajoute syles fieldset + exemple dans styleguide

--- web/app/themes/wwp_child_theme/styleguide/components/components/forms.php
+++ web/app/themes/wwp_child_theme/styleguide/components/components/forms.php
@@ -2,6 +2,13 @@
 <p class="subTitle">Formulaire de base</p>
 <?php echo do_shortcode("[wwpmodule slug='wwp-contact' form='1']"); ?>
 <br>
+<br>
+
+<p class="subTitle">Formulaire avec groupes de champs</p>
+<span class="help">Pour générer des groupes de champs, il faut ajouter l'intitulé des groupes dans les clés de traduction.</span>
+<div class="">
+    <?php echo do_shortcode("[wwpmodule slug='wwp-contact' form='5']"); ?>
+</div>
 
 <p class="subTitle">Formulaire labels masqués</p>
 <div class="form-nolabel">
