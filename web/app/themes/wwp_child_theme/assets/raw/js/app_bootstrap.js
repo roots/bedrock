6d62d635207371fa969ea5c80c4cbc17b1ab8227
Jeremy Desvaux <jeremy.desvaux@wonderful.fr>
2021-03-30 17:53:35 +0200

    polyfill replace array

--- web/app/themes/wwp_child_theme/assets/raw/js/app_bootstrap.js
+++ web/app/themes/wwp_child_theme/assets/raw/js/app_bootstrap.js
@@ -52,6 +52,16 @@ if (!String.prototype.includes) {
   };
 }
 
+String.prototype.replaceArray = function(find, replace) {
+  var replaceString = this;
+  var regex;
+  for (var i = 0; i < find.length; i++) {
+    regex = new RegExp(find[i], "g");
+    replaceString = replaceString.replace(regex, replace[i]);
+  }
+  return replaceString;
+};
+
 /**
  * Pew.js polyfill
  */
