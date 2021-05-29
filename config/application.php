b2781b890e5f988f847e716e6abbc48e34c3cc5c
Scott Walkinshaw <scott.walkinshaw@gmail.com>
2021-05-28 22:23:14 -0400

    Support .env.local config override (#594)
    If `.env.local` exists, any values will override and take precedence
    over those in `.env`.
    
    This is meant for local development only.

--- config/application.php
+++ config/application.php
@@ -27,8 +27,9 @@ $webroot_dir = $root_dir . '/web';
 
 /**
  * Use Dotenv to set required environment variables and load .env file in root
+ * .env.local will override .env if it exists
  */
-$dotenv = Dotenv\Dotenv::createUnsafeImmutable($root_dir);
+$dotenv = Dotenv\Dotenv::createUnsafeImmutable($root_dir, ['.env', '.env.local'], false);
 if (file_exists($root_dir . '/.env')) {
     $dotenv->load();
     $dotenv->required(['WP_HOME', 'WP_SITEURL']);
