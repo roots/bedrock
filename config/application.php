e9400200cdbdf00c97e7ad80802c48221ec7ac8d
QWp6t <QWp6t@users.noreply.github.com>
2021-05-28 19:45:26 -0700

    Default to `WP_DEBUG_LOG=true` in development (#505)
    Setting WP_DEBUG_LOG to `true` will cause WP to automatically log debug
    output to <WP_CONTENT>/debug.log (so app/debug.log) which is a sensible default.
    
    This also comments out the old placeholder value in `.env.example` since
    `true` is a better default.

--- config/application.php
+++ config/application.php
@@ -105,7 +105,7 @@ Config::define('WP_POST_REVISIONS', env('WP_POST_REVISIONS') ?: true);
  * Debugging Settings
  */
 Config::define('WP_DEBUG_DISPLAY', false);
-Config::define('WP_DEBUG_LOG', env('WP_DEBUG_LOG') ?? false);
+Config::define('WP_DEBUG_LOG', false);
 Config::define('SCRIPT_DEBUG', false);
 ini_set('display_errors', '0');
 
