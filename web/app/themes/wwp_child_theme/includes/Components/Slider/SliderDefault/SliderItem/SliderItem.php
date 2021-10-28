b95d54fa0fccd2a2f0ceeabf3ae5016104c0c159
Jeremy Desvaux <jeremy.desvaux@wonderful.fr>
2021-10-28 10:29:41 +0200

    [slider] Component interface refactoring.

--- web/app/themes/wwp_child_theme/includes/Components/Slider/SliderDefault/SliderItem/SliderItem.php
+++ web/app/themes/wwp_child_theme/includes/Components/Slider/SliderDefault/SliderItem/SliderItem.php
@@ -8,104 +8,20 @@
 
 namespace WonderWp\Theme\Child\Components\Slider\SliderDefault\SliderItem;
 
-use WonderWp\Theme\Child\Components\Slider\Interfaces\SlideInterface;
+use WonderWp\Plugin\Slider\Component\SlideComponentInterface;
+use WonderWp\Plugin\Slider\Component\TwigSlideComponentTrait;
 use WonderWp\Theme\Core\Component\TwigComponent;
 
-class SliderItem extends TwigComponent implements SlideInterface
+class SliderItem extends TwigComponent implements SlideComponentInterface
 {
-    /** @var string */
-    protected $img;
-    /** @var string */
-    protected $imgAlt;
-    /** @var string */
-    protected $title;
-    /** @var string */
-    protected $subtitle;    
-    /** @var string */
-    protected $link;
-    /** @var string */
-    protected $linkLabel;
-    /** @var string */
-    protected $content;
-
-    /** @inheritDoc */
-    public function setImg(string $img)
-    {
-        $this->__set('img', stripslashes($img));
-
-        return $this;
-    }
-
-    /** @inheritDoc */
-    public function setImgAlt(string $imgAlt)
-    {
-        $this->__set('imgAlt', stripslashes($imgAlt));
-
-        return $this;
-    }
-
-    /** @inheritDoc */
-    public function setTitle(string $title)
-    {
-        $this->__set('title', stripslashes($title));
-
-        return $this;
-    }
-
-    /** @inheritDoc */
-    public function setSubtitle(string $subtitle)
-    {
-        $this->__set('subtitle', stripslashes($subtitle));
-
-        return $this;
-    }
-
-    /** @inheritDoc */
-    public function setLink(string $link)
-    {
-        $this->__set('link', stripslashes($link));
-
-        return $this;
-    }
-
-    /** @inheritDoc */
-    public function setLinkLabel(string $label)
-    {
-        $this->__set('linkLabel', stripslashes($label));
-
-        return $this;
-    }
-
-    /** @inheritDoc */
-    public function setContent(string $content)
-    {
-        $this->__set('content', stripslashes($content));
-
-        return $this;
-    }
-
-    /** @inheritDoc */
-    public function setImgWidth(int $imgWidth)
-    {
-        $this->__set('imgWidth', stripslashes($imgWidth));
-
-        return $this;
-    }
-
-    /** @inheritDoc */
-    public function setImgHeight(int $imgHeight)
-    {
-        $this->__set('imgHeight', stripslashes($imgHeight));
-
-        return $this;
-    }
+    use TwigSlideComponentTrait;
 
     public function __construct($templatesDir = null, $templateName = null)
     {
-        if(empty($templatesDir)){
+        if (empty($templatesDir)) {
             $templatesDir = __DIR__;
         }
-        if(empty($templateName)){
+        if (empty($templateName)) {
             $templateName = 'slider-item';
         }
         parent::__construct($templatesDir, $templateName);
