77965b1984c730c79bde100783f2d0c97b50f4a4
Chauncey McAskill <chauncey@mcaskill.ca>
2022-09-02 13:38:01 -0400

    Refactor plugin that customizes Site Health test
    Replaced hooks closures with functions to make it easier to remove, replace, reuse, or extend if needed.
    
    Added type-hinting for static analysis tools (tested with PHPStan and Psalm).
    
    Improved description of test result to support potential edge case where the result data has no description during override process.

--- web/app/mu-plugins/bedrock-site-health-tests.php
+++ web/app/mu-plugins/bedrock-site-health-tests.php
@@ -14,38 +14,77 @@ namespace Roots\Bedrock;
 
 use WP_HTTP_Response;
 
-add_filter( 'site_status_test_result', function ( array $result ) : array {
-    if ( ! isset( $result['test'] ) || $result['test'] !== 'background_updates' ) {
+add_filter( 'site_status_test_result', __NAMESPACE__ . '\\filter_site_status_test_result' );
+add_filter( 'rest_post_dispatch', __NAMESPACE__ . '\\filter_rest_post_dispatch' );
+
+/**
+ * Filters the output for the Background updates test
+ * to override the result.
+ *
+ * @param  array<array-key, mixed> $result An associative array of test result data.
+ * @return array<array-key, mixed>
+ */
+function filter_site_status_test_result( array $result ) : array {
+    if (
+        ! isset( $result['test'] ) ||
+        $result['test'] !== 'background_updates'
+    ) {
         return $result;
     }
 
     return override_test_background_updates( $result );
-} );
+}
 
-add_filter( 'rest_post_dispatch', function ( WP_HTTP_Response $result ) : WP_HTTP_Response {
+/**
+ * Filters the REST API response for the Background updates test
+ * to override the result.
+ *
+ * @param  WP_HTTP_Response $result The REST API response.
+ * @return WP_HTTP_Response
+ */
+function filter_rest_post_dispatch( WP_HTTP_Response $result ) : WP_HTTP_Response {
     $data = $result->get_data();
-    if ( ! isset( $data['test'] ) || $data['test'] !== 'background_updates' ) {
+    if (
+        ! is_array( $data ) ||
+        ! isset( $data['test'] ) ||
+        $data['test'] !== 'background_updates'
+    ) {
         return $result;
     }
 
+    /** @var array<array-key, mixed> $data */
+
     $result->set_data( override_test_background_updates( $data ) );
     return $result;
-} );
+}
 
 /**
  * Overrides the result of the finished Background updates test
  * to avoid conflict with the way Bedrock is structured
  * (managed by Composer and version control system).
+ *
+ * @param  array<array-key, mixed> $result An associative array of test result data.
+ * @return array<array-key, mixed>
  */
-function override_test_background_updates( array $result ) : array
-{
+function override_test_background_updates( array $result ) : array {
+    $description = sprintf(
+        '<p>%s</p>',
+        __( 'This site is under version control. Updates are managed by Composer.' )
+    );
+
+    if (
+        isset( $result['description'] ) &&
+        is_string( $result['description'] )
+    ) {
+        $description .= sprintf(
+            '<blockquote class="notice notice-info">%s</blockquote>',
+            $result['description']
+        );
+    }
+
     return array_replace( $result, [
         'label'       => __( 'Background updates are disabled by Bedrock' ),
         'status'      => 'good',
-        'description' => sprintf(
-            '<p>%s</p><blockquote class="notice notice-info">%s</blockquote>',
-            __( 'This site is under version control. Updates are managed by Composer.' ),
-            $result['description']
-        ),
+        'description' => $description,
     ] );
 }
