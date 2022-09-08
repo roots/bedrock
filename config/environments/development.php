30b53f9fd6975fd19819b978390b8b631534c7f5
Andy Wilkinson <158435+LarkRiseMedia@users.noreply.github.com>
2022-09-08 17:30:36 +0100

    update .gitignore
    Remove .DS_Store from everywhere

--- config/environments/development.php
+++ config/environments/development.php
@@ -7,7 +7,7 @@ use Roots\WPConfig\Config;
 use function Env\env;
 
 Config::define('SAVEQUERIES', true);
-Config::define('WP_DEBUG', true);
+Config::define('WP_DEBUG', false);
 Config::define('WP_DEBUG_DISPLAY', true);
 Config::define('WP_DEBUG_LOG', env('WP_DEBUG_LOG') ?? true);
 Config::define('WP_DISABLE_FATAL_ERROR_HANDLER', true);
