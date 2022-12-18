bd68a5ce2de10f7df71f040e447fe6f20552d53d
Stefano Fasoli <67009812+TimeZHero@users.noreply.github.com>
2022-12-18 19:53:40 +0100

    fix: can disable revisions by setting false or 0 (#658)

--- config/application.php
+++ config/application.php
@@ -103,7 +103,7 @@ Config::define('DISALLOW_FILE_EDIT', true);
 // Disable plugin and theme updates and installation from the admin
 Config::define('DISALLOW_FILE_MODS', true);
 // Limit the number of post revisions that Wordpress stores (true (default WP): store every revision)
-Config::define('WP_POST_REVISIONS', env('WP_POST_REVISIONS') ?: true);
+Config::define('WP_POST_REVISIONS', env('WP_POST_REVISIONS') ?? true);
 
 /**
  * Debugging Settings
