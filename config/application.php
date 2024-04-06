b7af24125130630678f1dc093daf386209c19922
QWp6t <hi@qwp6t.me>
2024-04-06 16:59:30 -0700

    💥 use `Dotenv::createImmutable()`

--- config/application.php
+++ config/application.php
@@ -11,6 +11,9 @@
 use Roots\WPConfig\Config;
 use function Env\env;
 
+// USE_ENV_ARRAY + CONVERT_* + STRIP_QUOTES
+Env\Env::$options = 31;
+
 /**
  * Directory containing all of the site's files
  *
@@ -34,7 +37,7 @@ if (file_exists($root_dir . '/.env')) {
         ? ['.env', '.env.local']
         : ['.env'];
 
-    $dotenv = Dotenv\Dotenv::createUnsafeImmutable($root_dir, $env_files, false);
+    $dotenv = Dotenv\Dotenv::createImmutable($root_dir, $env_files, false);
 
     $dotenv->load();
 
